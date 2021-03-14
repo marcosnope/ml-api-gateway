22259c4a00e5cb1b65889b010dccffe539a3bbca
MacBook Pro <marcosnope13@gmail.com>
2021-03-14 12:28:26 -0500

    routes

--- src/routes/items/items.routes.js
+++ src/routes/items/items.routes.js
@@ -2,6 +2,9 @@ import { Router } from 'express';
 const router = Router();
 import itemServices from '../../services/items/items.services';
 
+router.get('/ping', (req, res) => {
+  return res.json({ message: 'pong' });
+});
 // api/items
 // Obtener productos
 router.get('/', itemServices.getItemsDescription);
